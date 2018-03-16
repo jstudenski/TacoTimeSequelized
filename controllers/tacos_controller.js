c2fd8e35f3d5b4ace6d78a75e4679233b1334061
jstudenski <jstudenski@gmail.com>
2018-03-16 13:05:17 -0500

    controller clean up

--- controllers/tacos_controller.js
+++ controllers/tacos_controller.js
@@ -14,84 +14,47 @@ router.get('/', function (req, res) {
   res.redirect('/index');
 });
 
-
-// Index Page (render all tacos to DOM)
+// RENDER
 router.get('/index', function (req, res) {
-
   models.tacos.findAll().then(function(data){
     console.log(data.length);
     var object = { tacos: data };
     res.render("index", object);
   })
-
 });
 
-
-// Create a New Burger
+// NEW
 router.post('/api/tacos', function (req, res) {
-
-  // Sequelize Query to add new burger to database
-  models.tacos.create(
-    {
-      taco_name: req.body.name
-    }
-  ).then(function(){
-    console.log("worked");
-    //res.json({ id: result.insertId });
-    res.redirect('/index');
+  models.tacos.create({
+    taco_name: req.body.name
+  }).then(function(dbPost){
+    res.json(dbPost);
   });
-
 });
 
-
+// UPDATE
 router.put("/api/tacos/:id", function(req, res) {
-
   models.tacos.update({
-    eaten: "1" },
-    {where: {id: req.params.id}}
-  ).then(function(dbPost) {
-   res.json(dbPost)
- })
-
+    eaten: "1" 
+  },{
+    where: {
+      id: req.params.id
+    }
+  }).then(function(dbPost) {
+    res.json(dbPost)
+  })
 });
 
-
-
-// router.delete("/api/tacos/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-//   models.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-
-
-  // DELETE route for deleting posts
+// DELETE
 router.delete("/api/tacos/:id", function(req, res) {
-    models.tacos.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbPost) {
-      res.json(dbPost);
-    });
+  models.tacos.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(dbPost) {
+    res.json(dbPost);
+  });
 });
-
-
 
 // Export routes for server.js to use.
 module.exports = router;
-
-// router.post("/api/tacos", function(req, res) {
-//   models.create([
-//     req.body.name
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//   });
-// });
-
